7fd1742e534526677397aa4ef550064838159a50
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2020-07-15 17:05:47 +0100

    Update screen recorder handler to fix ConcurrentModificationException

--- vendor/vendor-android/base/src/main/kotlin/com/malinskiy/marathon/android/executor/listeners/video/ScreenRecorderHandler.kt
+++ vendor/vendor-android/base/src/main/kotlin/com/malinskiy/marathon/android/executor/listeners/video/ScreenRecorderHandler.kt
@@ -1,14 +1,24 @@
 package com.malinskiy.marathon.android.executor.listeners.video
 
+import java.util.concurrent.CopyOnWriteArrayList
+
 class ScreenRecorderHandler {
 
-    private val listeners: MutableList<() -> Unit> = arrayListOf()
+    private val listeners: MutableList<() -> Unit> = CopyOnWriteArrayList()
+
+    @Volatile
+    private var stopped: Boolean = false
 
     fun stop() {
+        stopped = true
         listeners.forEach { it() }
     }
 
     fun subscribeOnStop(onStop: () -> Unit) {
-        listeners += onStop
+        if (stopped) {
+            onStop()
+        } else {
+            listeners += onStop
+        }
     }
 }
